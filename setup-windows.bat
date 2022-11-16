aee831c439e6f8c955906ade1f19faec4c125b4b
Nguyễn Văn Khánh <88880309+KhanhNguyen9872@users.noreply.github.com>
2022-11-16 22:23:02 +0700

    update

--- setup-windows.bat
+++ setup-windows.bat
@@ -5,6 +5,8 @@ echo 1. Open php.ini file in your client (not php.ini in there) and replace all 
 echo.
 echo 2. Open apache2.conf or httpd.conf, remove the string "Index" to improve security and add "ServerName 0.0.0.0" to the first line of the file!
 echo.
-echo 3. Start your server and Enjoy!
+echo 3. Remove install file (php.ini, setup.sh, README.md, README.png, LICENSE, setup-windows.bat)
+echo.
+echo 4. Start your server and Enjoy!
 echo.
 pause > NUL
