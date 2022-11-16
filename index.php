b769e9a422cad87375e0a148b0d80338c71812a6
Nguyễn Văn Khánh <88880309+KhanhNguyen9872@users.noreply.github.com>
2022-11-16 14:41:38 +0700

    add favicon.ico, title

--- index.php
+++ index.php
@@ -2,8 +2,10 @@
 require_once 'check.php';
 ?>
 <!DOCTYPE html>
+<title>Temp Upload</title>
 <html><body>
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<meta charset="UTF-8"/>
 <form action="upload.php" method="post" enctype="multipart/form-data">
   <?php
   echo "Select file to upload (Max: ". ini_get('post_max_size'). ")";
