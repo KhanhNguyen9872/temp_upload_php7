78849295650f0925ca2096affdb70f3c1eba16f3
Nguyễn Văn Khánh <88880309+KhanhNguyen9872@users.noreply.github.com>
2022-11-16 11:10:24 +0700

    scale width device, detect upload is enabled or not

--- index.php
+++ index.php
@@ -1,10 +1,16 @@
+<?php
+  $upload = ini_get('file_uploads');
+  if ($upload != "1") {
+    echo "File upload has been disabled on the server!";
+    exit();
+  }
+?>
 <!DOCTYPE html>
-<html>
-<body>
-
+<html><body>
+<meta name="viewport" content="width=device-width, initial-scale=2">
 <form action="upload.php" method="post" enctype="multipart/form-data">
   <?php
-  echo "Select image to upload (Max: ". ini_get('post_max_size'). ")";
+  echo "Select file to upload (Max: ". ini_get('post_max_size'). ")";
   ?>
   <br>
   <input type="file" name="fileToUpload" id="fileToUpload">
@@ -12,6 +18,4 @@
   <input type="submit" value="Upload" name="submit">
   </br>
 </form>
-
-</body>
-</html>
+</body></html>
